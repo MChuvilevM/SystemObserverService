13aac118bd4be69c4a7863fa5549b688256215f4
MChuvilevM <mishanyasha324@gmail.com>
2026-06-02 13:07:13 +0300

    Configure MetricSettings and update DI container

--- src/SystemObserver.Worker/Program.cs
+++ src/SystemObserver.Worker/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SystemObserver.Application.Models;
 using SystemObserver.Application.Services;
 using SystemObserver.Infrastructure;
-using SystemObserver.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// Добавляем биндинг настроек
+builder.Services.Configure<MetricSettings>(
+    builder.Configuration.GetSection("MetricSettings"));
 
 builder.Services.AddInfrastructure();
 builder.Services.AddSingleton<MetricProcessor>();
