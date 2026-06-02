1540ebed2768efe8e2a00030c07054a71e9894f6
MChuvilevM <mishanyasha324@gmail.com>
2026-06-02 13:36:20 +0300

    Update MetricProcessorTests with invalid metric filtering test case

--- tests/SystemObserver.Tests/MetricProcessorTests.cs
+++ tests/SystemObserver.Tests/MetricProcessorTests.cs
@@ -1,8 +1,10 @@
+using Moq;
 using SystemObserver.Application.Services;
 using SystemObserver.Domain.Interfaces;
 using SystemObserver.Domain.Models;
-using Moq;
 using Xunit;
+
+namespace SystemObserver.Tests;
 
 public class MetricProcessorTests
 {
@@ -16,5 +18,17 @@ public class MetricProcessorTests
         await processor.ProcessAsync(metric, CancellationToken.None);
 
         mockRepo.Verify(r => r.SaveMetricAsync(metric, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task ProcessAsync_ShouldNotCallRepository_WhenMetricIsInvalid()
+    {
+        var mockRepo = new Mock<IMetricRepository>();
+        var processor = new MetricProcessor(mockRepo.Object);
+        var invalidMetric = new SystemMetric("CPU", -1, DateTime.UtcNow);
+
+        await processor.ProcessAsync(invalidMetric, CancellationToken.None);
+
+        mockRepo.Verify(r => r.SaveMetricAsync(It.IsAny<SystemMetric>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 }
