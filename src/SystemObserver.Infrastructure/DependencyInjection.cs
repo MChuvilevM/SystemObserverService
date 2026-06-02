b992059f369bb1b86cd1591700a1124aea46a61c
MChuvilevM <mishanyasha324@gmail.com>
2026-06-02 13:43:14 +0300

    Register MetricSettings in DI container via AddInfrastructure

--- src/SystemObserver.Infrastructure/DependencyInjection.cs
+++ src/SystemObserver.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SystemObserver.Application.Models;
 using SystemObserver.Domain.Interfaces;
 using SystemObserver.Infrastructure.Repositories;
 
@@ -6,8 +8,9 @@ namespace SystemObserver.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<MetricSettings>(configuration.GetSection("MetricSettings"));
         services.AddSingleton<IMetricRepository, FileMetricRepository>();
         return services;
     }
