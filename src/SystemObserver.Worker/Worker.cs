5d6c2be2ace53ec97422defd386224c331986e4e
MChuvilevM <mishanyasha324@gmail.com>
2026-06-02 12:34:09 +0300

    Add Worker.cs file
    Created Worker.cs implementing BackgroundService to serve as the background execution unit

---
+++ src/SystemObserver.Worker/Worker.cs
@@ -0,0 +1,15 @@
+namespace SystemObserver.Worker;
+
+public class Worker(MetricProcessor processor, ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation("Worker started at: {time}", DateTimeOffset.Now);
+        
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(5000, stoppingToken);
+        }
+    }
+}
