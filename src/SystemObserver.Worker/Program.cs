f40ff4b134b92d180b9dde91d83ba552c7fc0720
MChuvilevM <mishanyasha324@gmail.com>
2026-06-02 12:28:07 +0300

    Update Worker to use DI and registered services
    Configured Program.cs to wire up Domain, Application, and Infrastructure layers using Dependency Injection

--- src/SystemObserver.Worker/Program.cs
+++ src/SystemObserver.Worker/Program.cs
@@ -1,5 +1,14 @@
-using Microsoft.Extensions.Hosting;
+using SystemObserver.Application.Services;
+using SystemObserver.Infrastructure;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// Подключаем наш слой инфраструктуры
+builder.Services.AddInfrastructure();
+// Регистрируем сервисы приложения
+builder.Services.AddSingleton<MetricProcessor>();
+// Регистрируем фоновую задачу
+builder.Services.AddHostedService<Worker>();
+
 using IHost host = builder.Build();
 host.Run();
