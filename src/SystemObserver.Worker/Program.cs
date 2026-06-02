80c6bc841e7d3102265c32811b089f94b3e574c7
MChuvilevM <mishanyasha324@gmail.com>
2026-06-02 13:10:24 +0300

    Remove invalid namespace from Program.cs and fix Worker registration

--- src/SystemObserver.Worker/Program.cs
+++ src/SystemObserver.Worker/Program.cs
@@ -3,16 +3,16 @@ using Microsoft.Extensions.Hosting;
 using SystemObserver.Application.Models;
 using SystemObserver.Application.Services;
 using SystemObserver.Infrastructure;
+using SystemObserver.Worker; // Этот using обязателен!
 
 var builder = Host.CreateApplicationBuilder(args);
 
-// Добавляем биндинг настроек
 builder.Services.Configure<MetricSettings>(
     builder.Configuration.GetSection("MetricSettings"));
 
 builder.Services.AddInfrastructure();
 builder.Services.AddSingleton<MetricProcessor>();
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddHostedService<Worker>(); // Теперь класс Worker найден
 
 using IHost host = builder.Build();
 host.Run();
